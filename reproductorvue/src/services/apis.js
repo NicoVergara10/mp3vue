cbd44211880ccfd3210ea540a969d2f37a3b5d36
Nicolas <nickov.nv@gmail.com>
2022-07-02 11:46:23 -0300

    Trabajo en vistas

---
+++ reproductorvue/src/services/apis.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const clienteApi = axios.create({
+    baseURL: 'https://musicbrainz.org/ws/2',
+});
+
+export default {
+    getCanciones(cancionBuscar){
+        return clienteApi.get('/recording?fmt=json&query=' + cancionBuscar);
+    }
+}
