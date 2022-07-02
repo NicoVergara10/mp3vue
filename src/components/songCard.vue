0b57362f48523f34252e600ac9fe85e86901791c
Nicolas <nickov.nv@gmail.com>
2022-07-02 14:43:16 -0300

    centrar div

--- src/components/songCard.vue
+++ src/components/songCard.vue
@@ -1,18 +1,17 @@
 <template>
   <div :key="cancion" v-for="cancion in canciones" class="playlist">
-    <p>
-      <span id="title">Nombre: </span>
-      {{ cancion.title }}
-    </p>
-    <p>
-      <span id="artist">Artista: </span>
-      {{ cancion["artist-credit"][0].name }}
-    </p>
-    <p>
-      <span id="time">Duración: </span>
-      {{ cancion.length / 1000 }}
-    </p>
-    <i class="uil uil-play-circle"></i>
+    <div class="info">
+      <h2>
+        {{ cancion.title }}
+      </h2>
+      <h3>
+        {{ cancion["artist-credit"][0].name }}
+      </h3>
+      <p>{{ (cancion.length / 1000 / 60).toFixed(2) }} mins</p>
+    </div>
+    <div class="img">
+      <i class="uil uil-align-letter-right"></i>
+    </div>
   </div>
 </template>
 <script>
@@ -25,3 +24,22 @@ export default {
   },
 };
 </script>
+<style scoped>
+.playlist {
+  width: 60%;
+  padding-left: 20%;
+  padding-right: 20%;
+  display: grid;
+  grid-template-columns: auto auto;
+}
+.info {
+  text-align: left;
+}
+.img {
+  padding-top: 25%;
+  padding-bottom: 25%;
+}
+.img > i {
+  font-size: 250%;
+}
+</style>
